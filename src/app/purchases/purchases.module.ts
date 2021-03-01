59a259c522d4a74cb18236ec68491003c4fabf91
AvaSax <o_yamkovaya@mail.ru>
2021-03-01 14:22:46 +0300

    output datas from database in purchase-list

--- src/app/purchases/purchases.module.ts
+++ src/app/purchases/purchases.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PurchasesRoutingModule } from './purchases-routing.module';
-import { PurchasesComponent } from './purchases.component';
 import { PurchaseListComponent } from './purchase-list/purchase-list.component';
 import { PurchaseEditComponent } from './purchase-edit/purchase-edit.component';
 
 
 @NgModule({
-  declarations: [PurchasesComponent, PurchaseListComponent, PurchaseEditComponent],
+  declarations: [PurchaseListComponent, PurchaseEditComponent],
   imports: [
     CommonModule,
-    PurchasesRoutingModule
+    PurchasesRoutingModule 
   ]
 })
 export class PurchasesModule { }
